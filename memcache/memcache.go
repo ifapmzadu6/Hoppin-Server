25cba49259a7185819d6849c9b5010730937a874
ifapmzadu6 <ifapmzadu6@gmail.com>
2015-05-02 20:14:03 +0900

    rename

--- memcache/memcache.go
+++ memcache/memcache.go
@@ -1,8 +1,8 @@
-package memcached
+package memcache
 
 import "github.com/bradfitz/gomemcache/memcache"
 
-func Open() {
-	mc := memcache.New("10.0.0.1:11211", "10.0.0.2:11211", "10.0.0.3:11212")
+func Open() memcache {
+	mc := memcache.New("localhost:11211")
 	mc.Set(&memcache.Item{Key: "foo", Value: []byte("my value")})
 }
