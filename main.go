d53e7e7da7992e5a2104947ccf43a0d0383fc3f0
ifapmzadu6 <ifapmzadu6@gmail.com>
2015-05-03 13:45:09 +0900

    Save Action

--- main.go
+++ main.go
@@ -9,8 +9,27 @@ import (
 	"./mysql"
 )
 
+/*
+ 15 type Action struct {
+	  16     Id      int
+	   17     VideoId string
+	    18     Type    ActionType
+		 19     Time    int
+		  20     Start   int
+		   21     End     int
+		    22 }
+			 23
+			  24 type ActionType struct {
+				   25     Id     int
+				    26     String string
+					 27 }
+*/
+
 func main() {
 	db, _ := mysql.Open()
+	at := mysql.ActionType{String: "twitter"}
+	a := mysql.Action{VideoId: "VideoId", Type: at, Time: 100, Start: 20, End: 30}
+	mysql.InsertAction(db, a)
 	defer mysql.Close(db)
 
 	mc := memcache.Open()
--- mysql/mysql.go
+++ mysql/mysql.go
@@ -15,6 +15,7 @@ func Open() (*sql.DB, error) {
 	db.Exec("USE hoppin")
 
 	CreateActionTable(db)
+	CreateActionTypeTable(db)
 
 	return db, nil
 }
@@ -22,21 +23,6 @@ func Open() (*sql.DB, error) {
 func Close(db *sql.DB) {
 	db.Close()
 }
-
-/*
-type Action struct {
-	Id      int
-	VideoId string
-	Type    ActionType
-	Time    uint16
-	Start   uint16
-	End     uint16
-}
-type ActionType struct {
-    Id     int
-    String string
-}
-*/
 
 func CreateDataBase(db *sql.DB) {
 	var sql = "CREATE DATABASE IF NOT EXISTS hoppin DEFAULT CHARACTER SET utf8;"
@@ -67,7 +53,10 @@ func CreateActionTable(db *sql.DB) {
 
 func CreateActionTypeTable(db *sql.DB) {
 	var sql = `
-	`
+	CREATE TABLE IF NOT EXISTS action_type (
+		id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+		string CHAR(32) UNIQUE NOT NULL
+	) DEFAULT CHARACTER SET utf8;`
 
 	_, err := db.Exec(sql)
 	if err != nil {
@@ -75,8 +64,11 @@ func CreateActionTypeTable(db *sql.DB) {
 	}
 }
 
-func InsertAction(a *Action) {
-	//var sql = "INSERT INTO video (id, videoId, type, time, start, end) value (?, ?, ?, ?)"
+func InsertAction(db *sql.DB, a Action) {
+	var sql = "INSERT INTO action (video_id, type, time, start, end) value (?, ?, ?, ?, ?)"
 
-	//db.Exec(sql, Action.VideoId, Action.Type, Action.Time, Action.Start, Action.End)
+	_, err := db.Exec(sql, a.VideoId, a.Type.Id, a.Time, a.Start, a.End)
+	if err != nil {
+		panic(err)
+	}
 }
