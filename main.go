06751cfc69752a69206ce49d9cf6de3b786e09db
ifapmzadu6 <ifapmzadu6@gmail.com>
2015-05-04 00:25:32 +0900

    DBの記述を戻した

--- main.go
+++ main.go
@@ -7,19 +7,17 @@ import (
 )
 
 func main() {
-	/*
-		err := mysql.Open()
-		if err != nil {
-			panic(err)
-		}
-		at := mysql.ActionType{String: "twitter"}
-		a := mysql.Action{VideoId: "VideoId", Type: at, Time: 100, Start: 20, End: 30}
-		mysql.InsertAction(a)
-		defer mysql.Close()
+	err := mysql.Open()
+	if err != nil {
+		panic(err)
+	}
+	at := mysql.ActionType{String: "twitter"}
+	a := mysql.Action{VideoId: "VideoId", Type: at, Time: 100, Start: 20, End: 30}
+	mysql.InsertAction(a)
+	defer mysql.Close()
 
-		mc := memcache.Open()
-		memcache.Set("value", "key", mc)
-	*/
+	mc := memcache.Open()
+	memcache.Set("value", "key", mc)
 
 	//v, _ := memcache.Get("key", mc)
 
