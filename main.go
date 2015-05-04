02c9e05500ff39b6291be60876197b048ec8504c
ifapmzadu6 <ifapmzadu6@gmail.com>
2015-05-04 22:03:53 +0900

    Check if exists cert.pem/key.pem

--- main.go
+++ main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"./actions"
 	"./index"
-	"./memcache"
-	"./mysql"
 )
 
 func main() {
@@ -22,12 +21,23 @@ func main() {
 
 	mc := memcache.Open()
 	memcache.Set("value", "key", mc)
-
 	//v, _ := memcache.Get("key", mc)
 
 	http.HandleFunc("/", index.Handler)
 	http.HandleFunc("/actions/", actions.Handler)
 
+	if Exists("/ssl/cert.pem") == false {
+		panic("/ssl/cert.pem is not exists.")
+	}
+	if Exists("/ssl/key.pem") == false {
+		panic("/ssl/key.pem is not exists.")
+	}
+
 	log.Println("Start ListenAndServe :8080")
 	http.ListenAndServeTLS(":8080", "/ssl/cert.pem", "/ssl/key.pem", nil)
+}
+
+func Exists(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil
 }
