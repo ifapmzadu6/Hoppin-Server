05d7ba99da1f7d9cd64fda6c6639c71d45bbc9a7
ifapmzadu6 <ifapmzadu6@gmail.com>
2015-05-05 15:56:46 +0900

    /action accept POST request only

--- actions/actions.go
+++ actions/actions.go
@@ -8,6 +8,10 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.NotFound(w, r)
+	}
+
 	decoder := json.NewDecoder(r.Body)
 	var as Actions
 	err := decoder.Decode(&as)
