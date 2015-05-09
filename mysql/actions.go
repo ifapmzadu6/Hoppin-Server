da5e4382730a8544831045b65ffba404925f655c
ifapmzadu6 <ifapmzadu6@gmail.com>
2015-05-09 23:29:55 +0900

    add index

--- mysql/actions.go
+++ mysql/actions.go
@@ -41,7 +41,8 @@ func createActionTypeTable(db *sql.DB) error {
 	var sql = `
 	CREATE TABLE IF NOT EXISTS action_types (
 		id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
-		name CHAR(32) UNIQUE NOT NULL
+		name CHAR(32) UNIQUE NOT NULL,
+		INDEX(name)
 	) DEFAULT CHARACTER SET utf8;`
 
 	_, err := db.Exec(sql)
